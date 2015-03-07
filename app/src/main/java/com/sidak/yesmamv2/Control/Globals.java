372210da70ea257bd65377c596299b1c7f90d505
sidak <spssachdeva21695@gmail.com>
2015-03-07 23:09:50 +0530

    Add java and xml file for splashscreen activity

---
+++ app/src/main/java/com/sidak/yesmamv2/Control/Globals.java
@@ -0,0 +1,16 @@
+package com.sidak.yesmamv2.Control;
+
+import com.sidak.yesmamv2.Model.UnmarkedClass;
+import com.sidak.yesmamv2.Model.WorkingDay;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sidak on 7/3/15.
+ */
+public class Globals {
+    public static WorkingDay workday= new WorkingDay();
+    public static List<UnmarkedClass> uClasses=new ArrayList<UnmarkedClass>();
+    public static boolean isUnmarkedClass=false;
+}
---
+++ app/src/main/java/com/sidak/yesmamv2/Control/WdayTemplateActivity.java
@@ -0,0 +1,7 @@
+package com.sidak.yesmamv2.Control;
+
+/**
+ * Created by sidak on 7/3/15.
+ */
+public class WdayTemplateActivity {
+}
